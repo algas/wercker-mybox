a381c329c09b0ca41952606410243e5fb103e1d5
Masahiro Yamauchi <sgt.yamauchi@gmail.com>
2017-02-04 21:36:04 +0900

    Update params

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,1 +1,3 @@
-mysql -h "$MYSQL_PORT_3306_TCP_ADDR" -P "$MYSQL_PORT_3306_TCP_PORT" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" < ./scripts/create_table.sql
+echo $MYSQL_PORT_3306_TCP_ADDR
+echo $MYSQL_PORT_3306_TCP_PORT
+mysql -h "$MYSQL_PORT_3306_TCP_ADDR" -P "$MYSQL_PORT_3306_TCP_PORT" -u "foo" -p"bar" "user" < ./scripts/create_table.sql
