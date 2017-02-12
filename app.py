2e90d049def33a9b8970f26974147e529f3b513b
Masahiro Yamauchi <sgt.yamauchi@gmail.com>
2017-02-12 16:29:06 +0900

    Add tests

--- app.py
+++ app.py
@@ -2,46 +2,52 @@ import tornado.ioloop
 import tornado.web
 import os
 import MySQLdb
+import logging
+import json
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
 
 class UserHandler(tornado.web.RequestHandler):
-    def __init__(self, connector):
+    def initialize(self, connector):
         self.connector = connector
 
     def get(self, uid):
-        cursor = self.connector.cursor()
+        cursor = self.connector.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute("select * from user")
-        self.write("<br>".join([row[1] for row in cursor.fetchall() if len(row) > 1]))
+        self.write(json.dumps(cursor.fetchall()))
         cursor.close()
 
-def make_app():
-    options = {
-        'mysql_host': os.environ.get('MYSQL_PORT_3306_TCP_ADDR', 'localhost'),
-        'mysql_port': int(os.environ.get('MYSQL_PORT_3306_TCP_PORT', 8888)),
-        'mysql_user': os.environ.get('MYSQL_ENV_MYSQL_USER', 'test'),
-        'mysql_pass': os.environ.get('MYSQL_ENV_MYSQL_PASSWORD', 'secret'),
-        'mysql_database': os.environ.get('MYSQL_ENV_MYSQL_DATABASE', 'user'),
-    }
-    connector = MySQLdb.connect(
-            host=options['mysql_host'],
-            port=options['mysql_port'],
-            user=options['mysql_user'],
-            passwd=options['mysql_pass'],
-            db=options['mysql_database'])
-
-    return tornado.web.Application([
-        (r"/", MainHandler),
-        (r"/user/(.*)", UserHandler, dict(connector=connector)),
-    ])
+class Application(tornado.web.Application):
+    def __init__(self, **kwargs):
+        options = {
+            'mysql_host': os.environ.get('MYSQL_PORT_3306_TCP_ADDR', 'localhost'),
+            'mysql_port': int(os.environ.get('MYSQL_PORT_3306_TCP_PORT', 8888)),
+            'mysql_user': os.environ.get('MYSQL_ENV_MYSQL_USER', 'test'),
+            'mysql_pass': os.environ.get('MYSQL_ENV_MYSQL_PASSWORD', 'secret'),
+            'mysql_database': os.environ.get('MYSQL_ENV_MYSQL_DATABASE', 'user'),
+        }
+        connector = MySQLdb.connect(
+                host=options['mysql_host'],
+                port=options['mysql_port'],
+                user=options['mysql_user'],
+                passwd=options['mysql_pass'],
+                db=options['mysql_database'])
+        handlers = [
+            (r"/", MainHandler),
+            (r"/users/(.*)", UserHandler, dict(connector=connector)),
+        ]
+        super(Application, self).__init__(handlers, **kwargs)
 
 def main(port):
-    app = make_app()
+    # app = make_app()
+    logging.debug("start server")
+    app = Application()
     app.listen(port)
     tornado.ioloop.IOLoop.current().start()
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     port = int(os.environ.get('PORT', '8888'))
     main(port)
--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,1 +1,1 @@
-mysql -h "$MYSQL_PORT_3306_TCP_ADDR" -P "$MYSQL_PORT_3306_TCP_PORT" -u "$MYSQL_ENV_MYSQL_USER" -p"$MYSQL_ENV_MYSQL_PASSWORD" -e "create table user(id int not null auto_increment, name varchar(255), primary key (id));" $MYSQL_ENV_MYSQL_DATABASE < scripts/create_table.sql
+mysql -h "$MYSQL_PORT_3306_TCP_ADDR" -P "$MYSQL_PORT_3306_TCP_PORT" -u "$MYSQL_ENV_MYSQL_USER" -p"$MYSQL_ENV_MYSQL_PASSWORD" -e "create table user(id int not null auto_increment, name varchar(255), primary key (id)); insert into user (name) values ('foo');" $MYSQL_ENV_MYSQL_DATABASE < scripts/create_table.sql
