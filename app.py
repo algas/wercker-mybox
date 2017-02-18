a49afac61d921b5bdcdb5760d4b97cf7e3057faf
Masahiro Yamauchi <sgt.yamauchi@gmail.com>
2017-02-18 13:08:50 +0900

    Implement crud1

--- app.py
+++ app.py
@@ -13,11 +13,38 @@ class UserHandler(tornado.web.RequestHandler):
     def initialize(self, connector):
         self.connector = connector
 
-    def get(self, uid):
+    def get(self, uid=None):
         cursor = self.connector.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute("select * from user")
+        if uid is None:
+            cursor.execute('select * from user')
+        else:
+            cursor.execute('select * from user where id = %s', uid)
         self.write(json.dumps(cursor.fetchall()))
         cursor.close()
+
+    def post(self):
+        name = self.get_argument('name')
+        cursor = self.connector.cursor(MySQLdb.cursors.DictCursor)
+        res = cursor.execute('insert into user (name) values (%(name)s)', {'name':name})
+        self.connector.commit()
+        self.write('insert user: %s' % name)
+        cursor.close()
+
+    def put(self, uid):
+        name = self.get_argument('name')
+        cursor = self.connector.cursor(MySQLdb.cursors.DictCursor)
+        res = cursor.execute('update user set name = %(name)s where id = %(uid)s', {'name':name, 'uid':uid})
+        self.connector.commit()
+        self.write('update user: %s' % name)
+        cursor.close()
+
+    def delete(self, uid):
+        cursor = self.connector.cursor(MySQLdb.cursors.DictCursor)
+        res = cursor.execute('delete from user where id = %(uid)s', {'uid':uid})
+        self.connector.commit()
+        self.write('delete user: %s' % uid)
+        cursor.close()
+
 
 class Application(tornado.web.Application):
     def __init__(self, **kwargs):
@@ -36,6 +63,7 @@ class Application(tornado.web.Application):
                 db=options['mysql_database'])
         handlers = [
             (r"/", MainHandler),
+            (r"/users", UserHandler, dict(connector=connector)),
             (r"/users/(.*)", UserHandler, dict(connector=connector)),
         ]
         super(Application, self).__init__(handlers, **kwargs)
@@ -43,7 +71,7 @@ class Application(tornado.web.Application):
 def main(port):
     # app = make_app()
     logging.debug("start server")
-    app = Application()
+    app = Application(autoreload=True)
     app.listen(port)
     tornado.ioloop.IOLoop.current().start()
 
